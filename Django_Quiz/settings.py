ef71fc2351dcf4bc13c29f076dd06a4aa08dfc89
IKROMOV DILSHOD <77174755+DeveloperBackendPro@users.noreply.github.com>
2024-01-13 04:02:30 +0500

    Update settings.py

--- Django_Quiz/settings.py
+++ Django_Quiz/settings.py
@@ -64,13 +64,23 @@ WSGI_APPLICATION = 'Django_Quiz.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': '5432',
     }
 }
-
 
 LANGUAGE_CODE = 'uz'
 
