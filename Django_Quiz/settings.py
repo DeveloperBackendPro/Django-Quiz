53a1a01c05d24922d7435aa9fe2bb138e963a89c
IKROMOV DILSHOD <77174755+DeveloperBackendPro@users.noreply.github.com>
2024-01-13 04:05:06 +0500

    Update settings.py

--- Django_Quiz/settings.py
+++ Django_Quiz/settings.py
@@ -7,7 +7,7 @@ env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-8rubnrpy8^dzs(ijet0h^@4v9lmqaq*ilgsq*%p+qz0w07j#u4'
+SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = False
 
